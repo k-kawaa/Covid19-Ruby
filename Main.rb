e021949c621f0123e89e70ce828ae4db51abc39d
Apartkktrain <kaisei.train@gmail.com>
2021-01-29 11:35:57 +0900

    Jsonデータ出力時の整形

--- Main.rb
+++ Main.rb
@@ -2,7 +2,11 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-uri = URI.parse('https://opendata.corona.go.jp/api/Covid19JapanAll?dataName=%E7%A5%9E%E5%A5%88%E5%B7%9D%E7%9C%8C')
-json = Net::HTTP.get(uri)
-result = JSON.parse(json)
-puts result
+def getData
+	uri = URI.parse('https://opendata.corona.go.jp/api/Covid19JapanAll?dataName=%E7%A5%9E%E5%A5%88%E5%B7%9D%E7%9C%8C')
+	json = Net::HTTP.get(uri)
+	result = JSON.parse(json)
+	result = JSON.pretty_generate(result)
+	puts result
+end
+getData
