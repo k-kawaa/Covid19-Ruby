6015d3a215696cc99ba29bc7f465beaf64151043
Apartkktrain <kaisei.train@gmail.com>
2021-01-29 11:49:47 +0900

    README修正*2

--- Main.rb
+++ Main.rb
@@ -2,11 +2,13 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-def getData
-	uri = URI.parse('https://opendata.corona.go.jp/api/Covid19JapanAll?dataName=%E7%A5%9E%E5%A5%88%E5%B7%9D%E7%9C%8C')
-	json = Net::HTTP.get(uri)
-	result = JSON.parse(json)
-	result = JSON.pretty_generate(result)
-	puts result
+class Main
+	def getData()
+		uri = URI.parse('https://opendata.corona.go.jp/api/Covid19JapanAll?dataName=%E7%A5%9E%E5%A5%88%E5%B7%9D%E7%9C%8C')
+		json = Net::HTTP.get(uri)
+		result = JSON.parse(json)
+		result = JSON.pretty_generate(result)
+		puts result
+	end
 end
-getData
+Main.new.getData
